535647ffd6d11f08f689dbd0ecc77ebd1e6c09d9
Peter Ajiboye <deuceclaver@gmail.com>
2025-11-13 22:04:08 +0100

    logout

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,5 +6,6 @@ const authController = require('../controllers/authController');
 router.post('/register', authController.register);
 router.post('/loginUser', authController.loginUser);
 router.post('/google', authController.googleLogin);
+router.post('/logout', authController.loginUser);
 
 module.exports = router;
